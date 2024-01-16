be437b4f9deddcee9fb0b8a227dae018b32398f4
Samuel <samuel.chiodini@unidavi.edu.br>
2024-01-16 09:53:43 -0300

    finalizado

--- security/src/main/java/tech/neckel/security/security/WebSecurityConfig.java
+++ security/src/main/java/tech/neckel/security/security/WebSecurityConfig.java
@@ -7,22 +7,25 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final CustomBasicAuthenticationFilter customBasicAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((request) -> request
                         .requestMatchers(HttpMethod.POST, "/user/**").permitAll()
-                        .anyRequest().authenticated());
-
-        return http.build();
+                        .anyRequest().authenticated()
+                ).addFilterBefore(customBasicAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
-
 }
--- security/src/main/java/tech/neckel/security/user/UserRepository.java
+++ security/src/main/java/tech/neckel/security/user/UserRepository.java
@@ -1,9 +1,13 @@
 package tech.neckel.security.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
 
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
+    User findByUsernameFetchRoles(@Param("username") String username);
 }
